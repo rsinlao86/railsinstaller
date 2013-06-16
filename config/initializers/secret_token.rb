27339275a4c0426d6454e84707b98df2a42eb40f
Ray <rsinlao86@rocketmail.com>
2013-06-15 23:01:56 -0700

    Added route

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsinstaller::Application.config.secret_token = 'bcdb2b1c3474d3e879a8b9a8e239d9e8ed58bc040a74c7d6fb47c84d381cf4627df749f97f68e1f80f9155b0b7e37a28513db178b684bf4c1722d7da911b429b'
